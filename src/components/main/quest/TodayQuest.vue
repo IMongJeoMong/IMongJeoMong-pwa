280008141c5ee4b2cbb1feaaf88387f65d9a8fad
wjdgusaho <wjdgusaho@gmail.com>
2023-07-02 05:11:45 +0900

    Upade 퀘스트 및 탐색하기 레이아웃 구성 및 기능 추가

--- src/components/main/quest/TodayQuest.vue
+++ src/components/main/quest/TodayQuest.vue
@@ -1,13 +1,204 @@
 <template>
-    <div>
-        
+    <div class="today_quest_box">
+        <div>
+            <div class="quest_list_box">
+                <div v-for="(quest, index) in todayQuest" :key="index" class="quest_box">
+                    <div class="quest_list_box_title">{{ quest.title }}</div>
+                    <div class="quest_list_box_compensattion">
+                        <div v-if="!quest.completionState">
+                            <div class="quest_list_compensation_exp_icon"></div>
+                            <div class="quest_list_compensation_exp_text">{{ quest.addExp}} EXP</div>
+                        </div>
+                        <div v-if="quest.completionState && !quest.getState" @click="rewarded(quest)"> <div>보상받기</div></div>
+                        <div v-if="quest.completionState && quest.getState" id="quest_end">완료</div>
+                    </div>
+                </div>
+            </div>
+            <div class="quest_compensation_box">
+                <div class="compensation_text">{{ todayQuest.length }}개 퀘스트 클리어 보상</div>
+                <div class="compensation_box">
+                    <div class="compensation_box_exp">
+                        <div class="compensation_box_exp_icon"></div>
+                        <div class="compensation_box_text">{{todayAllClear.exp}} Exp</div>
+                    </div>
+                    <div class="compensation_box_coin">
+                        <div class="compensation_box_coin_icon"></div>
+                        <div class="compensation_box_text">{{todayAllClear.coin}} p</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            todayQuest: [
+                {
+                    questCode: 200,
+                    title: "출석 하기",
+                    addExp: 10,
+                    completionState: true,
+                    getState: true,
+                },
+                {
+                    questCode: 300,
+                    title: "관광지 방문하기",
+                    addExp: 20,
+                    completionState: true,
+                    getState: false,
+                },
+                {
+                    questCode: 400,
+                    title: "리뷰 작성하기",
+                    addExp: 30,
+                    completionState: false,
+                    getState: false,
+                },
+            ],
+            todayAllClear: {
+                exp: 1000,
+                coin : 500,
+            }
+        }
+    },
+    methods: {
+        rewarded(quest) {
+            console.log(quest)
+            alert(`${quest.title} 보상 UI 만들어야합니다`);
+        }
+    }
 }
 </script>
 <style>
+    .today_quest_box > div{
+        margin:4% auto 5% auto;
+        width:90%;
+        height:91%;
+        border-radius: 10px;
+        background-color: #BBD4F8;
+    }
+
+    .quest_list_box{
+        height: 60%;
+        width: 100%;
+        border:1px solid rgba(255, 255, 255, 0);
+    }
     
+    .quest_box{
+        width: 90%;
+        height: 23%;
+        background-color:white;
+        margin: 5%;
+        border-radius: 10px;
+        display: flex;
+    }
+    
+    .quest_list_box_title{
+        width:70%;
+        font-size:17px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .quest_list_box_compensattion{
+        width:40%;
+        padding: 1%;
+    }
+    
+    .quest_list_box_compensattion > div {
+        background-color:#164C97;
+        border-radius: 15px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+    }
+
+    .quest_list_compensation_exp_icon{
+        width:20px;
+        height: 75%;
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-image: url("/src/assets/resource/common/img/icon/userLevelIcon.png");
+    }
+
+    .quest_list_compensation_exp_text{
+        text-align: center;
+        width: 45px;
+    }
+   
+
+
+    .quest_compensation_box{
+        width: 100%;
+        height: 40%;
+        /* border:1px solid black; */
+        padding:5%;
+        text-align: center;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+    }
+
+    .compensation_text{
+        color:#164C97;
+        font-size:19px;
+        font-weight: bold;
+    }
+
+
+    .compensation_box{
+        margin:3px auto;
+        width: 90%;
+        display:flex;
+        justify-content: space-around;
+        height:30px;
+        border-radius: 20px;
+        background-color: #164C97 ;
+        color:white;
+        font-size:12px;
+        font-weight: 500;
+    }
+
+    .compensation_box > div {
+        display: flex;
+        align-items: center;
+    }
+
+    .compensation_box > div > div{
+        height:20px;
+        text-align: right;
+        display:inline-block;
+        background-repeat: no-repeat;
+        background-size: 15px 15px;
+        background-position: top 2px right 5px;
+    }
+
+    .compensation_box_exp_icon{
+        width:20px;
+        padding-right:5%;
+        background-image: url("/src/assets/resource/common/img/icon/userLevelIcon.png");
+       
+    }
+
+    .compensation_box_coin_icon{
+        width:20px;
+        padding-right:5%;
+        background-image: url("/src/assets/resource/common/img/icon/coinIcon.png");
+    }
+
+    .compensation_box_text{
+        line-height: 20px;
+    }
+
+    #quest_end{
+        background-color: #ABABAB;
+    }
 </style>
--- src/components/map/MarkerSmallOverlay.vue
+++ src/components/map/MarkerSmallOverlay.vue
@@ -39,7 +39,7 @@ export default {
     } 
 }
 </script>
-<style>
+<style scoped> 
     .marker_overlay_small{
         left:2.5%;
         width: 95%;
