a0fb3364abdc173e2d66ee6a3c66cddb434266f0
ChanHwan <song960705@naver.com>
2023-07-10 20:25:04 +0900

    chore: baseURL 을 aws server ip주소로 변경

--- src/api/tokenHttp.js
+++ src/api/tokenHttp.js
@@ -3,44 +3,44 @@ import jwt_decode from "jwt-decode";
 import dayjs from "dayjs";
 
 // baseURL: process.env.VUE_APP_AWS_API_URL,
-const baseURL = "http://localhost:8800";
+// const baseURL = "http://localhost:8800";
+const baseURL = "http://3.34.77.175:8800";
 
 const tokenHttp = axios.create({
-    baseURL,
-    header: {
-        "Content-Type": "application/json",
-    }
+  baseURL,
+  header: {
+    "Content-Type": "application/json",
+  },
 });
 
 // 요청 인터셉터 설정
 tokenHttp.interceptors.request.use(async (req) => {
+  const accessToken = sessionStorage.getItem("access-token");
+  if (!accessToken) {
+    console.log("token 이 존재하지 않습니다.");
+    throw new Error("expire token");
+  }
 
-    const accessToken = sessionStorage.getItem("access-token");
-    if (!accessToken) {
-        console.log("token 이 존재하지 않습니다.");
-        throw new Error("expire token");
-    }
+  const user = jwt_decode(accessToken);
+  // 여기가 뭘까요 ..?
+  const isExpired = dayjs().diff(dayjs.unix(user.exp)) < 1;
 
-    const user = jwt_decode(accessToken);
-    // 여기가 뭘까요 ..?
-    const isExpired = dayjs().diff(dayjs.unix(user.exp)) < 1;
+  // access token 이 만료되지 않았다면 access-token 을 넣어 요청 실행
+  if (isExpired) {
+    req.headers["Authorization"] = `Bearer ${accessToken}`;
+    return req;
+  }
 
-    // access token 이 만료되지 않았다면 access-token 을 넣어 요청 실행
-    if (isExpired) {
-        req.headers["Authorization"] = `Bearer ${accessToken}`;
-        return req;
-    }
+  // 만료되었다면 refresh-token으로 token 재발급
+  console.log("api/tokenHttp.js : access token 만료");
 
-    // 만료되었다면 refresh-token으로 token 재발급
-    console.log("api/tokenHttp.js : access token 만료");
-    
-    await axios
+  await axios
     .post(
       `${baseURL}/user/refresh`,
       {},
       {
         headers: {
-          "Authorization": sessionStorage.getItem("refresh-token"),
+          Authorization: sessionStorage.getItem("refresh-token"),
         },
       }
     )
